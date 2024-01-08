141592345be4abd5fa5fea1cea877a41aa87f537
Max Franz <maxkfranz@gmail.com>
2024-01-08 12:26:38 -0500

    Add Tailwind & Tailwind UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
