afced55ce38155a3d1970355347c1724489c91a5
Christian Lopes <chrtannus@gmail.com>
2024-02-23 20:07:06 -0300

    Initial suggestion for the top section text -- #7

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -100,12 +100,11 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Invest at the perfect time.
+              Transform your bioinformatics research
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              By leveraging insights from our network of industry insiders,
-              you’ll know exactly when to buy to maximize profit, and exactly
-              when to sell to avoid painful losses.
+              Finding the perfect tool to search or create biological network figures has never been easier. 
+              Whether you are exploring genes, proteins or pathways, our intuitive interface allows you to analyze your data and create beautiful figures that are ready to be published.
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
