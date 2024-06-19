3e611f10b17c7cf29cef2527fc6afa958d64f0dd
Christian Lopes <chrtannus@gmail.com>
2024-06-19 16:52:23 -0300

    Fixes cropped dropdown over the wizard dialog (removes `overflow-hidden` from DialogPanel) -- #18

--- src/components/base/WizardDialog.jsx
+++ src/components/base/WizardDialog.jsx
@@ -37,7 +37,7 @@ export function WizardDialog({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <DialogPanel className="relative transform overflow-hidden w-full rounded-t-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-6 sm:rounded-lg">
+              <DialogPanel className="relative transform w-full rounded-t-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-6 sm:rounded-lg">
                 <div className="absolute right-0 top-0 pr-4 pt-4">
                   <button
                     type="button"
