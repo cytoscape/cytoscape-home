cd2b61a75a910b72cfb321d5854c731c24d25a04
Christian Lopes <chrtannus@gmail.com>
2024-07-17 17:35:02 -0300

    Reduces the line-height of the genes textarea -- #18

--- src/components/wizards/GeneWizard.jsx
+++ src/components/wizards/GeneWizard.jsx
@@ -34,11 +34,11 @@ function GenesPanel({ initialValue, onChange }) {
       </label>
       <div className="mt-2">
         <textarea
-          rows={6}
+          rows={7}
           name="genes"
           id="genes"
           value={value}
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-complement-500 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-complement-500 sm:text-sm sm:leading-5"
           onChange={handleChange}
         />
       </div>
