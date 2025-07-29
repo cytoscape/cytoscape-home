0b935933e50894ebc0bf899d6b3bf15dd63cd8e1
Christian Lopes <chrtannus@gmail.com>
2025-07-29 18:13:17 -0300

    Simpler "Try it now" button; CSS tweaks -- #6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,15 +14,6 @@ const searchExamples = [
   { label: 'glycolysis', terms: 'glycolysis' },
 ]
 
-const DNAIcon = (props) => (
-  <svg viewBox="0 -960 960 960" aria-hidden="true" {...props}>
-    <path
-      d="M200-40v-40q0-139 58-225.5T418-480q-102-88-160-174.5T200-880v-40h80v40q0 11 .5 20.5T282-840h396q1-10 1.5-19.5t.5-20.5v-40h80v40q0 139-58 225.5T542-480q102 88 160 174.5T760-80v40h-80v-40q0-11-.5-20.5T678-120H282q-1 10-1.5 19.5T280-80v40h-80Zm138-640h284q13-19 22.5-38t17.5-42H298q8 22 17.5 41.5T338-680Zm142 148q20-17 39-34t36-34H405q17 17 36 34t39 34Zm-75 172h150q-17-17-36-34t-39-34q-20 17-39 34t-36 34ZM298-200h364q-8-22-17.5-41.5T622-280H338q-13 19-22.5 38T298-200Z"
-      fill={props.fill || '#000000'}
-    />
-  </svg>
-)
-
 
 export function Hero({ onGetStarted }) {
   const [searchText, setSearchText] = useState('')
@@ -48,7 +39,7 @@ export function Hero({ onGetStarted }) {
   }
 
   return (
-    <div className="overflow-hidden py-5 lg:py-20">
+    <div className="overflow-hidden py-5 lg:pt-20">
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
@@ -60,25 +51,18 @@ export function Hero({ onGetStarted }) {
               Whether you are exploring genes, proteins or pathways, our intuitive interface allows you to analyze your data and create beautiful figures that are ready to be published.
             </p>
             <div className="mt-10 flex flex-col gap-y-0 sm:items-start xs:items-start">
-              <span className="isolate inline-flex mb-5">
-                <div
-                  color="gray"
-                  className="relative -mb-5 py-2 pl-3 pr-5 inline-flex items-center rounded-t-md bg-gray-800 text-white justify-center text-sm font-semibold"
-                >
-                  <DNAIcon
-                    fill="#b5b5b5"
-                    aria-hidden="true"
-                    className="size-5 mr-3"
-                  />
-                  Try it now!
-                </div>
-              </span>
+              <div
+                color="gray"
+                className="relative py-2 pl-5 pr-6 inline-flex items-center text-gray-200 bg-gray-900 rounded-t-md justify-center text-sm font-semibold drop-shadow-md"
+              >
+                Try it now:
+              </div>
               <SearchBar
                 placeholder="Enter one or more genes, a pathway or any terms"
                 initialText={searchText}
                 onTextChange={handleTextChange}
                 onSubmit={onGetStarted}
-                className="rounded-none rounded-b-md rounded-r-md"
+                className="rounded-none rounded-b-md rounded-r-md bg-white drop-shadow-md"
               />
               <div className="mt-2 text-sm text-gray-500">
                 <span className="mr-2">Examples:</span>
