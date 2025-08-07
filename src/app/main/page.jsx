cdc671f8fccd260b9fd2a3cf2df2299dfcb80e7b
Christian Lopes <chrtannus@gmail.com>
2025-08-07 11:05:24 -0300

    Closes the results dialog on browser's back navigation -- #32

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
 import { Hero } from '@/components/Hero'
@@ -15,8 +15,37 @@ export default function Home({ searchEngine }) {
   const [searchType, setSearchType] = useState('gene')
   const [searchText, setSearchText] = useState('')
   const [openGuide, setOpenGuide] = useState(false)
-  const [openResults, setOpenResults] = useState(false)
+  const [resultsOpen, setResultsOpen] = useState(false)
   const [results, setResults] = useState()
+
+  const openResults = () => {
+    if (!window.history.state?.resultsOpen) {
+      window.history.pushState({ resultsOpen: true }, '#results', window.location.href) // Push a new state to the history stack
+    }
+    setResultsOpen(true)
+  }
+  const closeResults = () => {
+    setResultsOpen(false)
+    // // Prevent the browser from navigating back to the results state when the user clicks the forward button
+    // if (window.location.hash === '#results') {
+    //   window.history.replaceState({}, document.title, window.location.pathname + window.location.search)
+    // }
+  }
+
+  useEffect(() => {
+    // Listen for popstate events to handle back/forward navigation
+    const handlePopState = (event) => {
+      if (event.state && event.state.resultsOpen) {
+        setResultsOpen(true)
+      } else {
+        setResultsOpen(false)
+      }
+    }
+    window.addEventListener('popstate', handlePopState)
+    return () => {
+      window.removeEventListener('popstate', handlePopState)
+    }
+  }, [])
 
   const handleSubmit = (data) => {
     setOpenGuide(false)
@@ -25,12 +54,12 @@ export default function Home({ searchEngine }) {
         window.open(data.url, '_blank').focus()
       } else {
         setResults(data)
-        setOpenResults(true)
+        openResults()
       }
     }
   }
   const handleGetStarted = ({ type, terms }) => {
-    setOpenResults(false)
+    setResultsOpen(false)
     setResults(null)
     setSearchType(type)
     setSearchText(terms ? terms.join(' ') : '')
@@ -48,7 +77,7 @@ export default function Home({ searchEngine }) {
       <Citations />
       <Faqs />
       <Guide open={openGuide} type={searchType} initialText={searchText} onClose={() => setOpenGuide(false)} onSubmit={handleSubmit} />
-      <Results open={openResults} data={results} searchEngine={searchEngine} onClose={() => setOpenResults(false)} />
+      <Results open={resultsOpen} data={results} searchEngine={searchEngine} onClose={closeResults} />
     </>
   )
 }
